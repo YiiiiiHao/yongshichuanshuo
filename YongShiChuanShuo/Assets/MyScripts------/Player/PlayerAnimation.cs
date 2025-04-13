7e6f47591156f8f10f1e49295561e12aea65c398
YiiiiiHao <102990539+YiiiiiHao@users.noreply.github.com>
2025-04-14 03:27:50 +0800

    YongShiChuanShuo_V1

--- YongShiChuanShuo/Assets/MyScripts------/Player/PlayerAnimation.cs
+++ YongShiChuanShuo/Assets/MyScripts------/Player/PlayerAnimation.cs
@@ -6,10 +6,12 @@ public class PlayerAnimation : MonoBehaviour
 { 
     private Animator anim;
     private Rigidbody2D rb;
+    private PhysicsCheck physicsCheck;//跳跃动作需要知道是否在地面上，所以需要获取PhysicsCheck组件里的isGround属性
     void Awake()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        physicsCheck = GetComponent<PhysicsCheck>();
     }
     void Update()
     {
@@ -20,6 +22,8 @@ public class PlayerAnimation : MonoBehaviour
         //由于在animatro中是大于0.1就跑步，但是向左跑会是-1，所以用Mathf.Abs(rb.velocity.x)来获取绝对值
         //这样向左向右跑就是大于0.1.
         anim.SetFloat("velocityX", Mathf.Abs(rb.velocity.x));
-    
+        //根据y轴速度来设置跳跃动画
+        anim.SetFloat("velocityY", rb.velocity.y);//y轴速度，跳跃
+        anim.SetBool("isGround", physicsCheck.isGround);//是否在地面上
     }
 }
