fb935803416377dcee4cfe22c5672b3eab99f72f
YiiiiiHao <102990539+YiiiiiHao@users.noreply.github.com>
2025-07-20 14:31:39 +0800

    YongShiChuanShuo_V1

--- YongShiChuanShuo/Assets/MyScripts------/Enemy/Enemy.cs
+++ YongShiChuanShuo/Assets/MyScripts------/Enemy/Enemy.cs
@@ -7,20 +7,34 @@ public class Enemy : MonoBehaviour
 {
     Rigidbody2D rb;
     protected Animator anim;
+    PhysicsCheck physicsCheck;
     [Header("基本参数")]
     public float normalSpeed;
     public float chaseSpeed;
     public float currentSpeed;
     public Vector3 faceDir;
+    [Header("转身计时器")]
+    public float waitTime;
+    public float waitTimeCounter=0;
+    public bool wait;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        physicsCheck = GetComponent<PhysicsCheck>();
         currentSpeed = normalSpeed;
+        waitTimeCounter = waitTime;
     }
     void Update()
     {
         faceDir = new Vector3(-transform.localScale.x, 0, 0);//设置朝向
+
+        //撞墙转身
+        if ((physicsCheck.touchRightWall && faceDir.x > 0) || (physicsCheck.touchLeftWall && faceDir.x < 0))
+        {
+            wait = true;
+        }
+        TimeCounter();
     }
     void FixedUpdate()
     {
@@ -29,6 +43,21 @@ public class Enemy : MonoBehaviour
 
     public virtual void Move()
     {
+        //速度*方向*时间。Y方向不动
         rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
+    }
+
+    public void TimeCounter()
+    {
+        if (wait)
+        {
+            waitTimeCounter -= Time.deltaTime;//设置的时间，减去时间修正。
+        }
+        if (waitTimeCounter <= 0)
+        {
+            wait = false;
+            waitTimeCounter = waitTime;//时间归0
+            transform.localScale = new Vector3(faceDir.x, 1, 1);
+        }
     }
 }
--- YongShiChuanShuo/Assets/MyScripts------/General/PhysicsCheck.cs
+++ YongShiChuanShuo/Assets/MyScripts------/General/PhysicsCheck.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
-{       
+{
+    private CapsuleCollider2D coll;
     [Header("检测参数")]
+    public bool manual;//是否手动控制
     public Vector2 bottomOffset;//位移差值
-    public Vector2 LeftOffset;//左墙偏移
-    public Vector2 RightOffset;//右墙偏移
+    public Vector2 LeftOffset;//左墙检测偏移
+    public Vector2 RightOffset;//右墙检测偏移
     public float checkRadius = 3f;//检测半径
     public LayerMask groundLayer;//地面层
     [Header("状态")]
     public bool isGround;//是否站在地面
     public bool touchLeftWall;//是否碰撞到左墙
     public bool touchRightWall;//是否碰撞到右墙
+    void Awake()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+        if (!manual)
+        {
+            ///自动计算位移差值
+            RightOffset = new Vector2((coll.bounds.size.x + coll.offset.x) / 2, coll.bounds.size.y / 2);
+            LeftOffset = new Vector2(-RightOffset.x, RightOffset.y);
+        }
+    }
     void Update()
     {
         Check();
